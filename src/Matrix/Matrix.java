e1cb98bc3cd09ae696dc3f736db7387d54323cbf
shidqizh <shidqizh@gmail.com>
2022-09-25 12:16:19 +0700

    d

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -14,6 +14,17 @@ public class Matrix{
         this.row = row;
         this.matrix = new Double [row][col];
     }
+    public Matrix(int row, int col, double c){
+        this.row = row;
+        this.col = col;
+        for(int i = 0; i < this.row; i++){
+          for(int j = 0; j < this.col; j++){
+            this.matrix[i][j] = c;
+          }
+        }
+      }
+
+      
 }
 
 

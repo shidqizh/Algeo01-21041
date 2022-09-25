1e5b79b921d69264ed24b3ba60685c416ec79b2c
shidqizh <shidqizh@gmail.com>
2022-09-25 13:40:15 +0700

    determinan kofaktor

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -38,6 +38,15 @@ public class Matrix{
         return this.col;
     }
 
+    public void setElmt(double x, int i, int j){
+        this.matrix[i][j] = x;
+    }
+
+    public int countElmt(){
+        return this.row * this.col;
+    }
+
+
     public int getFirstIdx(int i){
       int idx=0;
       for (int j=0; j<getCol();j++) {
@@ -53,7 +62,7 @@ public class Matrix{
       int idx = getFirstIdx(i);
       double firstElmt = getElmt(i, idx);
       for (int j = idx; j<getCol();j++) {
-        getElmt(i, j) = getElmt(i,j) / firstElmt;
+        this.matrix[i][j] = getElmt(i,j) / firstElmt;
       }
     }
 
@@ -61,11 +70,63 @@ public class Matrix{
       double temp;
       for (int j = 0; j<getCol();j++) {
         temp = getElmt(i1, j);
-        getElmt(i1, j) = getElmt(i2,j);
-        getElmt(i2, j) = temp;
+        this.matrix[i1][j] = getElmt(i2,j);
+        this.matrix[i2][j] = temp;
       }
     }
 
+    public double cofac(Matrix m, int r, int c){
+        int i;
+        int j;
+        int ni;
+        int nj;
+        double cof;
+        Matrix nm;
+        ni = 0;
+        cof = 0;
+        nm = new Matrix(m.getRow()-1,m.getCol()-1);
+        for (i=0;i<=m.getRow()-1;i++){
+            nj = 0;
+            for (j=0;j<=m.getCol()-1;j++){
+                if (i == r){
+                    ni = i - 1;
+                } 
+                else if (j == c){
+                    nj = j - 1;
+                } 
+                else{
+                    nm.setElmt(m.getElmt(i, j),ni,nj);
+                }
+                nj++;
+            }
+            ni++;
+        }
+    
+        if ((r + c) % 2 == 0){
+            cof = determinant(nm);
+        } 
+        else{
+            cof = -1*determinant(nm);
+        }
+    
+        return cof;
+    }
+
+    public double determinant(Matrix m){
+        int i;
+        int j;
+        double det = 0;
+        if(m.countElmt() == 1){
+            return m.getElmt(0, 0);
+        }
+        else{
+            i = 0;
+            for (j=0;j<=m.getCol()-1;j++){
+                det += m.getElmt(i, j) * cofac(m,i,j);
+            }
+        }
+        return det;
+    }
 
 }
 

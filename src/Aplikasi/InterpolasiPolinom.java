aad8a4a9b6b4570a9dcde9d0a7864aceb484df0e
muhhanan <16521166@std.stei.itb.ac.id>
2022-09-27 21:05:27 +0700

    interpolinom

---
+++ src/Aplikasi/InterpolasiPolinom.java
@@ -0,0 +1,43 @@
+package Aplikasi;
+
+import java.util.Scanner;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
+import Matrix.*;
+
+public class InterpolasiPolinom {
+    Scanner scan = new Scanner(System.in);
+    public InterpolasiPolinom(){
+
+    }
+    
+    public void InpolPolinom(){
+        int n, i, j, x, y;
+        System.out.println("Masukkan jumlah titik yang ingin dimasukkan: ");
+        n = scan.nextInt();
+        Matrix tmpM = new Matrix(n, n+1);
+        Matrix tmpH = new Matrix(n, n+1);
+
+        for (i=0; i<n; i++){
+            System.out.print("Masukkan x[" + i + "] y[" + i + "] = ");
+            x = scan.nextInt();
+            y = scan.nextInt();
+            for (j=0; j<n; j++){
+                tmpM.setElmt(Math.pow(x,j), i, j);
+            }
+            tmpM.setElmt(y, i, n);
+        }
+        System.out.println("");
+        System.out.println("==============================");
+        System.out.println("");
+        tmpM.displayMatrix();
+        System.out.println("");
+
+        tmpH = tmpM.gaussJordan();
+        System.out.println("Solusi dari interpolasi polinom tersebut adalah");
+        for (i=0; i<n; i++){
+            System.out.println("a[" + i +"] = " + tmpH.getElmt(i, n));
+        }
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,17 +1,21 @@
 import Matrix.*;
+import Aplikasi.InterpolasiPolinom;
+import Aplikasi.InterpolasiPolinom.*;
 
 public class main {
     public static void main(String[] agrs) {
-        Matrix m = new Matrix(4, 6);
-        m.readMatrix();
-        m.displayMatrix();
+        //Matrix m = new Matrix(4, 6);
+        //m.readMatrix();
+        //m.displayMatrix();
 
         //double det = m.detGauss();
         //System.out.println(det);
 
-        Matrix p = new Matrix(4,6);
-        p = m.gaussJordan();
-        p.displayMatrix();
+        //Matrix p = new Matrix(4,6);
+        //p = m.gaussJordan();
+        //p.displayMatrix();
         
+        InterpolasiPolinom tmp = new InterpolasiPolinom();
+        tmp.InpolPolinom();
     }
 }
